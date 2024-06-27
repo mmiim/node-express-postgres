9fd26aaebb88b9199191369cea493439fe108bdd
mmiim <166688207+mmiim@users.noreply.github.com>
2024-06-27 07:17:34 +0000

    15の変更の途中

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,40 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-
-  router.get('/', function (req, res, next) {
-    knex("tasks")
-      .select("*")
-      .then(function (results) {
-        console.log(results);
-        res.render('index', {
-          title: 'ToDo App',
-          todos: results,
-        });
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+router.get('/', function (req, res, next) {
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
       });
-  });
-
-  router.post('/', function (req, res, next) {
-    const todo = req.body.add;
-    knex("tasks")
-      .insert({user_id: 1, content: todo})
-      .then(function () {
-        res.redirect('/')
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
       });
-  });
+    });
+});
+
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
+});
+
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
